cc4ecaf134a8b4c7c67f4ca7f1b1a130c7db198c
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2021-11-02 17:06:26 -0300

    Experimento desde 201901 hasta 202010

--- segunda_kaggle/rfiles/823_epic.r
+++ segunda_kaggle/rfiles/823_epic.r
@@ -54,13 +54,13 @@ kapply_mes       <- c(202011)  #El mes donde debo aplicar el modelo
 
 ktrain_subsampling  <- 0.1   #el undersampling que voy a hacer de los continua
 
-ktrain_mes_hasta    <- 202002  #Obviamente, solo puedo entrenar hasta 202011
-ktrain_mes_desde    <- 201907
+ktrain_mes_hasta    <- 202010  #Obviamente, solo puedo entrenar hasta 202011
+ktrain_mes_desde    <- 201901
 
 ktrain_meses_malos  <- c()  #meses que quiero excluir del entrenamiento
 
-kgen_mes_hasta    <- 202002  #Obviamente, solo puedo entrenar hasta 202011
-kgen_mes_desde    <- 201907
+kgen_mes_hasta    <- 202010  #Obviamente, solo puedo entrenar hasta 202011
+kgen_mes_desde    <- 201901
 
 
 kBO_iter    <-  150   #cantidad de iteraciones de la Optimizacion Bayesiana
