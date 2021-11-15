76e27c90e7f08b28d1b102954450d50e92f05d29
Joaquín Ignacio Aramendía <samsagax@gmail.com>
2021-11-15 09:45:42 -0300

    Fix params

--- segunda_kaggle/stacking/847_epic_stacking.r
+++ segunda_kaggle/stacking/847_epic_stacking.r
@@ -72,7 +72,7 @@ hs <- makeParamSet(
          makeNumericParam("lambda_l1",        lower=    0.0  , upper= 100.0),
          makeNumericParam("lambda_l2",        lower=    0.0  , upper= 200.0),
          makeIntegerParam("min_data_in_leaf", lower=   10L   , upper= 8000L),
-         makeIntegerParam("num_leaves",       lower=    8L   , upper= 1024L),
+         makeIntegerParam("num_leaves",       lower=    8L   , upper= 1024L)
          #makeIntegerParam("max_bin",          lower=    3L   , upper= 100L)
         )
 
